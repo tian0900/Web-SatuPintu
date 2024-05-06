ec8d006e373a23755ab6eb7b3dff4e3b4f0f02bf
otniel687 <80443010+otniel687@users.noreply.github.com>
2024-05-06 10:14:06 +0700

    Push Pop Up

--- resources/views/data/atribut.blade.php
+++ resources/views/data/atribut.blade.php
@@ -255,44 +255,10 @@
                                     </td>
                                     <td class="px-6 py-4">
                                         <!-- Modal toggle -->
-                                        <a data-modal-target="modal<?= $item->id ?>"
-                                            data-modal-toggle="modal<?= $item->id ?>"
-                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline text-center">Edit</a>
-                                            <a data-modal-target="modalhapus<?= $item->id ?>" data-modal-toggle="modalhapus<?= $item->id ?>" class="font-medium text-red-600 dark:text-red-500 hover:underline text-center">Hapus</a>
+                                        <a data-modal-target="modal<?= $item->id ?>" data-modal-toggle="modal<?= $item->id ?>" class="font-medium text-blue-600 dark:text-blue-500 hover:underline text-center">Edit</a>
+                                        <a data-modal-target="modalhapus<?= $item->id ?>" data-modal-toggle="modalhapus<?= $item->id ?>" class="font-medium text-red-600 dark:text-red-500 hover:underline text-center">Hapus</a>
                                     </td>
                                 </tr>
-
-
-                                  <div id="modalhapus<?= $item->id ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                        <div class="relative p-4 w-full max-w-md max-h-full">
-                            <!-- Modal content -->
-                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                <!-- Modal header -->
-                                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                        Hapus Data
-                                    </h3>
-                                    <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modal<?= $item->id ?>">
-                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                                        </svg>
-                                        <span class="sr-only">Close modal</span>
-                                    </button>
-                                </div>
-                                <!-- Modal body -->
-                                <div class="p-4">
-                                    <form action="{{ route('sampah.destroy', $item->id) }}" method="POST">
-                                        @csrf
-                                        @method('Delete')
-                                        <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                            <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                                            Hapus
-                                        </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div> 
 
                                 <!-- Main modal -->
                                 <div id="modal<?= $item->id ?>" tabindex="-1" aria-hidden="true"
@@ -394,6 +360,41 @@
                                         </div>
                                     </div>
                                 </div>
+
+                                <!-- Main modal -->
+                                <div id="modalhapus<?= $item->id ?>" tabindex="-1" aria-hidden="true"
+                                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                    <div class="relative p-4 w-full max-w-md max-h-full">
+                                        <!-- Modal content -->
+                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <!-- Modal header -->
+                                            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modalhapus<?= $item->id ?>">
+                                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                                </svg>
+                                                <span class="sr-only">Close modal</span>
+                                            </button>
+                                            
+                                            <!-- Modal body -->
+                                            <div class="p-4 text-center ">
+                                                <form action="{{ route('sampah.destroy', $item->id) }}" method="POST">
+                                                    @csrf
+                                                    @method('Delete')
+                                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                                    </svg>
+                                                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this data?</h3>                                
+                                                    <button type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                                        Yes, I'm sure
+                                                    </button>
+                                                    <button data-modal-hide="modalhapus<?= $item->id ?>" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+
                             @endforeach
                         </tbody>
                     </table>
--- resources/views/data/kontrak.blade.php
+++ resources/views/data/kontrak.blade.php
@@ -257,18 +257,12 @@
                                     <td class="px-6 py-4">
                                         {{ $data->status }}
                                     </td>
-                                    <td class="px-6 py-4">
+                                    <td class="px-6 py-4 text-center">
                                         <a href="{{ route('surat.detail', ['id' => $data->id]) }}"
-                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline text-center">Detail</a>
-                                        <form action="{{ route('kontrak.delete', ['id' => $data->id]) }}"
-                                            method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Hapus Kontrak</button>
-                                        </form>
+                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline text-center">Detail</a><br>
+                                        <a data-modal-target="modalhapus<?= $data->id ?>" data-modal-toggle="modalhapus<?= $data->id ?>" class="font-medium text-red-600 dark:text-red-500 hover:underline text-center">Hapus Kontrak</a>
 
                                         <!-- Modal toggle -->
-
                                         @if ($data->status == 'DITERIMA')
                                             <span
                                                 class="font-medium text-blue-400 dark:text-blue-300 cursor-not-allowed">Konfirmasi</span>
@@ -277,8 +271,6 @@
                                                 data-modal-toggle="modalkonfirmasi{{ $data->id }}"
                                                 class="font-medium text-blue-600 dark:text-blue-500 hover:underline text-center">Konfirmasi</a>
                                         @endif
-
-
                                     </td>
                                     </td>
                                 </tr>
@@ -316,6 +308,39 @@
                                         </div>
                                     </div>
                                 </form>
+
+                                <!--Modal Hapus-->
+                                <div id="modalhapus<?= $data->id ?>" tabindex="-1" aria-hidden="true"
+                                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                    <div class="relative p-4 w-full max-w-md max-h-full">
+                                        <!-- Modal content -->
+                                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <!-- Modal header -->
+                                            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="modalhapus<?= $data->id ?>">
+                                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                                </svg>
+                                                <span class="sr-only">Close modal</span>
+                                            </button>
+                                            
+                                            <!-- Modal body -->
+                                            <div class="p-4 text-center ">
+                                                <form action="{{ route('kontrak.delete', ['id' => $data->id]) }}" method="POST">
+                                                    @csrf
+                                                    @method('Delete')
+                                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                                    </svg>
+                                                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this data?</h3>                                
+                                                    <button type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                                        Yes, I'm sure
+                                                    </button>
+                                                    <button data-modal-hide="modalhapus<?= $data->id ?>" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             @endforeach
                         </tbody>
                     </table>
